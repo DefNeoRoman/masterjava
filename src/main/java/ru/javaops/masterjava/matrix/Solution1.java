e2a5c20b5b85053922f19e0d9880c9823c34a354
DefNeoRoman <d8905611060@yandex.ru>
2018-03-12 17:36:42 +0300

    add bench

--- src/main/java/ru/javaops/masterjava/matrix/Solution1.java
+++ src/main/java/ru/javaops/masterjava/matrix/Solution1.java
@@ -1,29 +1,50 @@
 package ru.javaops.masterjava.matrix;
 
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.util.Random;
+
 public class Solution1 {
     private static final int MATRIX_SIZE = 10;
-    //всегда умножаем две квадратные матрицы, строку на столбец, проверки не требуется
+    //всегда умножаем две квадратные матрицы, строку на столбец, результаты складываем это будет один элемент, проверки не требуется
 
-    public static void main(String[] args) {
-        final int[][] matrixA = {{0,1,2,3,4},{0,1,2,3,4},{0,1,2,3,4},{0,1,2,3,4},{0,1,2,3,4}};
-        final int[][] matrixB = MatrixUtil.create(MATRIX_SIZE);
-        for (int[] aMatrixA : matrixA) {
-            for (int j = 0; j < matrixA.length; j++) {
-                System.out.print(aMatrixA[j] + " ");
+    @Benchmark
+    public  int [][] multiply(int[][] matrixA, int[][] matrixB ){
+        int [][] result = new int [matrixA.length][matrixB.length];
+        for (int i=0; i<matrixA.length; i++){
+            for (int j=0; j<matrixB.length; j++){
+                    for(int k=0;k<matrixA.length;k++){
+                        result[i][j] += matrixA[i][k]*matrixB[k][j];
+                    }
+            }
+        }
+        return result;
+    }
+    public  void printMatrix(int [][] matrix){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length;j++){
+                System.out.print(matrix[i][j] +", ");
             }
             System.out.println();
         }
     }
-    public static void multiply(int[][] matrixA, int[][] matrixB ){
-       for (int i=0; i<matrixA.length; i++){
-           for (int j=0; j<matrixA.length; j++){
-
-           }
-       }
+    public  int [][] transponeMatrix(int [][] matrix){
+        int [][] nMatrix = new int[matrix.length][matrix.length];
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix.length; j++){
+                nMatrix[i][j] = matrix[j][i];
+            }
+        }
+        return nMatrix;
     }
-    public static int[][] transpon(int[][] matrix){
-        int[][] newMatrix = new int[matrix.length][matrix.length];
-
-        return new int[10][10];
+    public  int [][] createMatrix(int rows){
+        Random random = new Random();
+        int [][] matrix = new int[rows][rows];
+        for(int i =0; i<rows; i++){
+            for (int j=0; j<rows; j++){
+                matrix[i][j] = random.nextInt(100000);
+            }
+        }
+        return matrix;
     }
 }
